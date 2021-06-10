98adb446ad9883fa5f595cec72d23aca19d03d4d
Matheus Almeida <mat.almeida@live.com>
2021-06-10 00:29:44 -0300

    feat: fix time to local timezone befor parse

--- src/lib/date.py
+++ src/lib/date.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, time, timedelta, timezone
 import re
 
 
-def create_togglr_date(date):
+def get_local_timezone_offset():
     now = datetime.now().astimezone()
 
     timezone_offset = now.strftime("%z")
@@ -10,32 +10,79 @@ def create_togglr_date(date):
     hours = timezone_offset[1:3]
     minutes = timezone_offset[3:5]
 
-    signal = "+" if signal == "-" else "-"
+    return {
+        "signal": signal,
+        "hours": hours,
+        "minutes": minutes,
+    }
 
-    timezone_offset = f"{signal}{hours}:{minutes}"
 
-    return f"{date}T00:00:00{timezone_offset}"
+def create_togglr_date(date):
+    timezone_offset = get_local_timezone_offset()
+
+    timezone_offset_str = (
+        f"{timezone_offset['signal']}"
+        + f"{timezone_offset['hours']}:"
+        + f"{timezone_offset['minutes']}"
+    )
+
+    return f"{date}T00:00:00{timezone_offset_str}"
+
+
+__DATE_REGEX = (
+    "(?P<year>\d{4})-"
+    + "(?P<month>\d{2})-"
+    + "(?P<day>\d{2})T"
+    + "(?P<hours>\d{2}):"
+    + "(?P<minutes>\d{2}):"
+    + "(?P<seconds>\d{2})"
+    + "(?P<offset_signal>[+-])"
+    + "(?P<offset_hours>\d{2}):"
+    + "(?P<offset_minutes>\d{2})"
+)
+
+
+def change_date_to_local_offset(date):
+    match = re.search(__DATE_REGEX, date)
+
+    if match == None:
+        return date
+    else:
+        local_off_set = get_local_timezone_offset()
+        local_offset_signal = local_off_set["signal"]
+        local_offset_hours = local_off_set["hours"]
+        local_offset_minutes = local_off_set["minutes"]
+
+        date_obj = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
+
+        local_offset_in_seconds = int(local_offset_hours) * 60 * 60
+        local_offset_in_seconds += int(local_offset_minutes) * 60
+
+        if local_offset_signal == "+":
+            date_obj += timedelta(seconds=local_offset_in_seconds)
+        else:
+            date_obj -= timedelta(seconds=local_offset_in_seconds)
+
+        return (
+            date_obj.strftime("%Y-%m-%dT%H:%M:%S%z")[:19]
+            + f"{local_offset_signal}{local_offset_hours}:{local_offset_minutes}"
+        )
 
 
 def togglr_date_to_dict(date: str):
-    match = re.search(
-        "(?P<year>\d{4})-"
-        + "(?P<month>\d{2})-"
-        + "(?P<day>\d{2})T"
-        + "(?P<hours>\d{2}):"
-        + "(?P<minutes>\d{2}):"
-        + "(?P<seconds>\d{2})",
-        date,
-    )
+    date_with_local_offset = change_date_to_local_offset(date)
+    print("=> date_with_local_offset", date_with_local_offset)
+
+    match = re.search(__DATE_REGEX, date_with_local_offset)
 
     if match == None:
         return {
-            "year": 0000,
-            "month": 00,
-            "day": 00,
-            "hours": 00,
-            "minutes": 00,
-            "seconds": 00,
+            "year": "0000",
+            "month": "00",
+            "day": "00",
+            "hours": "00",
+            "minutes": "00",
+            "seconds": "00",
         }
     else:
         return {
