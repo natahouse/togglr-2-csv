429cbd5fe6a15e52fb4fef30d42d92d51c9fcb06
Matheus Almeida <mat.almeida@live.com>
2021-07-15 22:53:53 -0300

    feat: add functions to retrive username

--- src/lib/credentials.py
+++ src/lib/credentials.py
@@ -1,6 +1,6 @@
 from .config_file import get_config, save_to_config
 
-__CREDENTIALS_KEYS = {"api_token": "API_TOKEN"}
+__CREDENTIALS_KEYS = {"api_token": "API_TOKEN", "username": "USERNAME"}
 
 
 def set_api_token(api_token):
@@ -17,3 +17,17 @@ def get_api_token():
     config = get_config()
 
     return config[__CREDENTIALS_KEYS["api_token"]]
+
+def set_username(username):
+    username_key = __CREDENTIALS_KEYS["name"]
+
+    config = get_config()
+
+    config[username_key] = username
+
+    save_to_config(config)
+
+def get_username():
+    config = get_config()
+
+    return config[__CREDENTIALS_KEYS["username"]]
